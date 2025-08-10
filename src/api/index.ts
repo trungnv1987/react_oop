3f86432796c7fcd3ef6050e8c365b529e864ccba
trung nguyen <trungnv2908@gmail.com>
2025-08-10 10:52:04 +0700

    refactor: centralize exports

--- src/api/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./apis/get_word_api";
-export * from "./apis/get_words_api";
-export * from "./apis/save_forms_api";
-export * from "./apis/save_meanings_api";
-export * from "./apis/save_phrasal_verbs_api";
-export * from "./apis/save_words_api";
-export * from "./base/api_param";
--- src/components/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './loading/ui_loading_abstract';
